65315bd91310c3c0e8433fcfeed425664bef6eac
Hikmahx <hikmayousuph@gmail.com>
2024-10-09 10:55:21 +0100

    community.js set up

---
+++ src/resources/community.js
@@ -0,0 +1,7 @@
+// Available subsection list for Community.js: ['General', 'Open Source', 'Women In Tech', 'Developers', 'Designers', 'Testers', 'Cloud', 'Mentorship']
+// Available tags list for Community.js: ['collaboration', 'networking', 'learning', 'support', 'career growth', 'womenintech', 'diversity', 'meetup', 'slack', 'discord', 'telegram']
+// The contributor must choose only one subsection from the list and can only add a new one if no suitable option exists.
+
+const communities = [
+
+];
