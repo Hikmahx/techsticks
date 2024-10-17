cd295d32a25bb7927d99c082f1f5aa062fad420a
Hikmahx <hikmayousuph@gmail.com>
2024-10-17 14:47:57 +0100

    margin added

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
  
 export default function NotFound() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-[50vh] text-center container px-4'>
+    <div className='flex flex-col items-center justify-center min-h-[50vh] text-center container px-4 m-auto'>
       <h2 className='font-quicksand text-2xl lg:text-4xl font-bold mb-8'>Not Found</h2>
       <p className='text-muted-foreground'>Could not find requested resource or page is currently in development</p>
     </div>
