40825db52fde98e4bf3a3ea4918ec50521e12616
Hikmahx <hikmayousuph@gmail.com>
2024-10-18 21:11:59 +0100

    style updated

--- src/app/resources/[slug]/page.tsx
+++ src/app/resources/[slug]/page.tsx
@@ -38,55 +38,48 @@ export default function ResourcePage({ params }: { params: { slug: string } }) {
           {resource.name}
         </h1>
       </div>
-      {/* {resource.icon && (
-        <Image
-          src={resource.icon}
-          alt={`${resource.name} icon`}
-          width={80}
-          height={80}
-          className='w-16 h-16 mb-6'
-        />
-      )} */}
       {resource.resources && resource.resources.length > 0 ? (
-        <div className='space-y-8 mt-12'>
-          {resource.resources.map((item, index) => (
-            <Card key={index} className='border pb-6 relative max-w-[18rem]'>
-              <div className='absolute -top-8 mt-4 left-6'>
-                {item.imageUrl ? (
-                  <div className='w-10 h-10 rounded-full bg-primary z-10 flex items-center justify-center'>
-                    <img
-                      src={item.imageUrl}
-                      alt={item.title}
-                      className='w-6 h-6 max-w-md m-auto'
-                    />
-                  </div>
-                ) : (
-                  <div className='w-4 h-4 rounded-full bg-primary/75'>
-                    {resource.name.charAt(0)}
-                  </div>
-                )}
-              </div>
-              <CardContent className='mt-6 py-2'>
-                <CardTitle className='text-2xl font-semibold mb-2 font-quicksand'>
-                  {item.title}
-                </CardTitle>
-                <p className='mb-2 text-sm'>{item.description}</p>
-                <a
-                  href={item.link}
-                  className='text-blue-900 text-xs font-bold hover:underline mb-2 block'
-                >
-                  {item.link}
-                </a>
-                <p className='text-xs text-gray-600'>Level: {item.level}</p>
-                <p className='text-xs text-gray-600'>
-                  Subsection: {item.subsection}
-                </p>
-              </CardContent>
-              <CardFooter className='flex items-center space-x-2 mt-4 pb-0'>
-                {item.tags.map(tagList)}
-              </CardFooter>
-            </Card>
-          ))}
+        <div className='product-container max-w-2xl mx-auto lg:max-w-7xl px-4 lg:px-0 flex items-center'>
+          <div className='gap-8 mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 flex-wrap items-center justify-center'>
+            {resource.resources.map((item, index) => (
+              <Card key={index} className='border pb-6 relative max-w-[18rem]'>
+                <div className='absolute -top-8 mt-4 left-6'>
+                  {item.imageUrl ? (
+                    <div className='w-10 h-10 rounded-full bg-primary z-10 flex items-center justify-center'>
+                      <img
+                        src={item.imageUrl}
+                        alt={item.title}
+                        className='w-6 h-6 max-w-md m-auto'
+                      />
+                    </div>
+                  ) : (
+                    <div className='w-4 h-4 rounded-full bg-primary/75'>
+                      {resource.name.charAt(0)}
+                    </div>
+                  )}
+                </div>
+                <CardContent className='mt-6 py-2'>
+                  <CardTitle className='text-2xl font-semibold mb-2 font-quicksand'>
+                    {item.title}
+                  </CardTitle>
+                  <p className='mb-2 text-sm'>{item.description}</p>
+                  <a
+                    href={item.link}
+                    className='text-blue-900 text-xs font-bold hover:underline mb-2 block'
+                  >
+                    {item.link}
+                  </a>
+                  <p className='text-xs text-gray-600'>Level: {item.level}</p>
+                  <p className='text-xs text-gray-600'>
+                    Subsection: {item.subsection}
+                  </p>
+                </CardContent>
+                <CardFooter className='flex items-center space-x-2 mt-4 pb-0'>
+                  {item.tags.map(tagList)}
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
         </div>
       ) : (
         <p>No resources available for this category.</p>
